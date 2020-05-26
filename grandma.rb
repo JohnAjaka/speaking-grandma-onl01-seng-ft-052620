b7dfd8eaa564619f3171aa598264cd418654b062
John Ajaka <johnajaka96@gmail.com>
2020-05-26 23:24:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elseif string = string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else
+  elsif
     return "NO, NOT SINCE 1938!"
   end
 end
